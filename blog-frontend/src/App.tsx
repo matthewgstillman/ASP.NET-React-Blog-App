877f9e0d6adcad3038e224b761855c7edfe18996
matthewgstillman <matthewgstillman@gmail.com>
2024-12-13 19:50:45 -0700

    Create Post component update, debugging

--- blog-frontend/src/App.tsx
+++ blog-frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import CreatePost from "./components/CreatePost";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App : React.FC = () => {
   return (
     <div className="App">
       <h1>Main Page</h1>
+      <CreatePost/>  
     </div>
   );
 }
---
+++ blog-frontend/src/components/CreatePost.tsx
@@ -0,0 +1,66 @@
+import React, { useState, FormEvent } from "react";
+import API from "../api";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const CreatePost : React.FC = () => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [author, setAuthor] = useState("");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        API.post("/posts", { title, content, author }).then(() => {
+            setTitle("");
+            console.log(title);
+            setContent("");
+            console.log(content);
+            setAuthor("");
+            console.log(author);
+            alert("Post created");
+        })
+    };
+    
+    return (
+        <div className="formContainer">
+            <h1>Create a new blog post</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formTitle">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Enter the title"
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formContent">
+                    <Form.Label>Content</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="Enter the content"
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formAuthor">
+                    <Form.Label>Author</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                        placeholder="Enter the author name"
+                    />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Create Post
+                </Button>
+            </Form>
+        </div>
+    );
+}
+
+export default CreatePost;
